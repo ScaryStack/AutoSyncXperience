2e42cedf8920b92ebebcd19245c68357c166db71
Patricio <patriciojimenez838@gmail.com>
2025-06-08 17:34:30 -0400

    Estructura básica de microservicios

---
+++ disponibilidad/disponibilidad/src/test/java/com/taller/disponibilidad/DisponibilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taller.disponibilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisponibilidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
