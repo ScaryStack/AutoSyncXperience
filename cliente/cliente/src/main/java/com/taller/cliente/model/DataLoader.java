48643a85f4e2f128f1c6674e9733e9e4a1ba6502
Patricio <patriciojimenez838@gmail.com>
2025-06-17 15:51:34 -0400

    Crea DataLoader para añadir clientes con perfil de prueba

---
+++ cliente/cliente/src/main/java/com/taller/cliente/model/DataLoader.java
@@ -0,0 +1,42 @@
+package com.taller.cliente.model;
+
+import com.taller.cliente.repository.ClienteRepository;
+import net.datafaker.Faker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Random;
+
+@Profile("test")
+@Component
+public class DataLoader implements CommandLineRunner {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Faker faker = new Faker();
+        Random random = new Random();
+
+        // Generar clientes
+        for (Integer i = 0; i < 50; i++) {
+            Cliente cliente = new Cliente();
+            cliente.setIdCliente(i + 1);
+            cliente.setPrNombre(faker.name().firstName());
+            cliente.setSegNombre(faker.name().firstName());
+            cliente.setTerNombre(faker.name().firstName());
+            cliente.setApPaterno(faker.name().lastName());
+            cliente.setApMaterno(faker.name().lastName());
+            cliente.setCorreo(faker.internet().emailAddress());
+            cliente.setTelefono(faker.number().numberBetween(100000000, 999999999));
+
+            clienteRepository.save(cliente);
+        }
+
+        List<Cliente> clientes = clienteRepository.findAll();
+    }
+}
