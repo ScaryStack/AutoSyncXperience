439b4272ac9dc6f2c0d4ede9492d5e472a1766f3
benja <benj.moralesb@duocuc.cl>
2025-06-30 02:49:14 -0400

    se crea VehiculoModelAssembler.java

---
+++ vehiculo/vehiculo/src/main/java/com/taller/vehiculo/assembler/VehiculoModelAssembler.java
@@ -0,0 +1,20 @@
+package com.taller.vehiculo.assembler;
+
+
+import com.taller.vehiculo.controller.VehiculoControllerV2;
+import com.taller.vehiculo.model.Vehiculo;
+import org.springframework.stereotype.Component;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+
+@Component
+public class VehiculoModelAssembler implements RepresentationModelAssembler<Vehiculo, EntityModel<Vehiculo>> {
+    @Override
+    public EntityModel<Vehiculo> toModel(Vehiculo vehiculo) {
+        return EntityModel.of(vehiculo,
+                linkTo(methodOn(VehiculoControllerV2.class).getVehiculoById(vehiculo.getIdVehiculo())).withSelfRel(),
+                linkTo(methodOn(VehiculoControllerV2.class).getAllVehiculos()).withRel("vehicilos"));
+
+    }
+}
